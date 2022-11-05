636da5531a0ee81c9d22aaf4bbaec68809516dca
nok <nok3zy@gmail.com>
2022-11-05 21:12:02 +0900

    unary operation alias test

--- src/test/kotlin/BinaryOperationSpec.kt
+++ src/test/kotlin/BinaryOperationSpec.kt
@@ -1,5 +1,6 @@
 import Funtions.FunctionBasedSpec
 import Funtions.IntBiOp
+import Funtions.IntUnOp
 import com.appmattus.kotlinfixture.kotlinFixture
 import io.kotest.matchers.shouldBe
 
@@ -17,6 +18,21 @@ class BinaryOperationSpec : FunctionBasedSpec() {
 
                 then("Result will be a plus b") {
                     val expected = a + b
+
+                    result shouldBe expected
+                }
+            }
+        }
+
+        given("A Int value") {
+            val a = fixture<Int>()
+
+            `when`("Square it") {
+                val square: IntUnOp = { it * it }
+
+                val result = square(a)
+                then("Result will be a * a") {
+                    val expected = a * a
 
                     result shouldBe expected
                 }
