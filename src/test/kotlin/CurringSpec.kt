081d21d5e1a78b52e478b3e224a0c6e6825af0fe
nok3zy <nok3zy@gmail.com>
2022-11-07 11:00:06 +0900

    part one

--- src/test/kotlin/CurringSpec.kt
+++ src/test/kotlin/CurringSpec.kt
@@ -1,7 +1,9 @@
 import Funtions.FunctionBasedSpec
 import Funtions.fourArgumentsCurring
+import Funtions.partOne
 import io.kotest.matchers.shouldBe
 import io.kotest.property.checkAll
+import io.kotest.property.forAll
 
 class CurringSpec : FunctionBasedSpec() {
     init {
@@ -10,6 +12,14 @@ class CurringSpec : FunctionBasedSpec() {
                 val result = fourArgumentsCurring<String, String, String, String>()(a)(b)(c)(d)()
                 val expected = "$a $b $c $d"
                 result shouldBe expected
+            }
+        }
+
+        "One argument with function"{
+            val tax = { a: Int -> { b: Double -> a * (1 + b / 100) } }
+
+            forAll<Int, Double> { a, b ->
+                a != 0 || partOne(a, tax)(b) == tax(a)(b)
             }
         }
     }
--- src/test/kotlin/Funtions/Curring.kt
+++ src/test/kotlin/Funtions/Curring.kt
@@ -1,6 +1,6 @@
 package Funtions
 
-fun <A, B, C, D> FunctionBasedSpec.fourArgumentsCurring()= { a: A ->
+fun <A, B, C, D> FunctionBasedSpec.fourArgumentsCurring() = { a: A ->
     { b: B ->
         { c: C ->
             { d: D ->
@@ -11,3 +11,5 @@ fun <A, B, C, D> FunctionBasedSpec.fourArgumentsCurring()= { a: A ->
         }
     }
 }
+
+fun <A, B, C> FunctionBasedSpec.partOne(a: A, f: (A) -> (B) -> C): (B) -> C = f(a)
