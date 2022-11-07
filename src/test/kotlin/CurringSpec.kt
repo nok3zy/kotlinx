ff9b5b71669c2c3f630a350d8f4dcd7d35d47a52
nok3zy <nok3zy@gmail.com>
2022-11-07 15:58:20 +0900

    swapping 2 argument

--- src/test/kotlin/CurringSpec.kt
+++ src/test/kotlin/CurringSpec.kt
@@ -1,13 +1,17 @@
 import Funtions.FunctionBasedSpec
+import Funtions.curry
 import Funtions.fourArgumentsCurring
 import Funtions.partOne
 import Funtions.partTwo
+import Funtions.swapArg
 import io.kotest.matchers.shouldBe
 import io.kotest.property.checkAll
 import io.kotest.property.forAll
 
 class CurringSpec : FunctionBasedSpec() {
     init {
+        val tax = { a: Int -> { b: Double -> a * (1 + b / 100) } }
+
         "4ArgumentsCurring" {
             checkAll<String, String, String, String> { a, b, c, d ->
                 val result = fourArgumentsCurring<String, String, String, String>()(a)(b)(c)(d)()
@@ -17,18 +21,30 @@ class CurringSpec : FunctionBasedSpec() {
         }
 
         "Fixed money then input rate"{
-            val tax = { a: Int -> { b: Double -> a * (1 + b / 100) } }
-
             forAll<Int, Double> { a, b ->
                 a != 0 || partOne(a, tax)(b) == tax(a)(b)
             }
         }
 
         "Fixed rate then input money"{
-            val tax = { a: Int -> { b: Double -> a * (1 + b / 100) } }
-
             forAll<Int, Double> { a, b ->
                 a != 0 || partTwo(b, tax)(a) == tax(a)(b)
+            }
+        }
+
+        "Curry with two argument"{
+            val add = { a: Int, b: Int -> a + b }
+
+            forAll<Int, Int> { a, b ->
+                curry(add)(a)(b) == add(a, b)
+            }
+        }
+
+        "Argument swapping"{
+            val f = { a: Int, b: Double -> a * (1 + b / 100) }
+
+            forAll<Int, Double> { a, b ->
+                a != 0 || swapArg(curry(f))(b)(a) == f(a, b)
             }
         }
     }
--- src/test/kotlin/Funtions/Curring.kt
+++ src/test/kotlin/Funtions/Curring.kt
@@ -15,3 +15,7 @@ fun <A, B, C, D> FunctionBasedSpec.fourArgumentsCurring() = { a: A ->
 fun <A, B, C> FunctionBasedSpec.partOne(a: A, f: (A) -> (B) -> C): (B) -> C = f(a)
 
 fun <A, B, C> FunctionBasedSpec.partTwo(b: B, f: (A) -> (B) -> C): (A) -> C = { a: A -> f(a)(b) }
+
+fun <A, B, C> FunctionBasedSpec.curry(f: (A, B) -> C): (A) -> (B) -> C = { a -> { b -> f(a, b) } }
+
+fun <T, U, V> FunctionBasedSpec.swapArg(f: (T) -> (U) -> (V)): (U) -> (T) -> (V) = { u -> { t -> f(t)(u) } }
