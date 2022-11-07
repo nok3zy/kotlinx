3baa439af89a976158f20833895b418845e7f46e
nok3zy <nok3zy@gmail.com>
2022-11-07 11:19:26 +0900

    add curring example

--- src/test/kotlin/CurringSpec.kt
+++ src/test/kotlin/CurringSpec.kt
@@ -1,6 +1,7 @@
 import Funtions.FunctionBasedSpec
 import Funtions.fourArgumentsCurring
 import Funtions.partOne
+import Funtions.partTwo
 import io.kotest.matchers.shouldBe
 import io.kotest.property.checkAll
 import io.kotest.property.forAll
@@ -15,11 +16,19 @@ class CurringSpec : FunctionBasedSpec() {
             }
         }
 
-        "One argument with function"{
+        "Fixed money then input rate"{
             val tax = { a: Int -> { b: Double -> a * (1 + b / 100) } }
 
             forAll<Int, Double> { a, b ->
                 a != 0 || partOne(a, tax)(b) == tax(a)(b)
+            }
+        }
+
+        "Fixed rate then input money"{
+            val tax = { a: Int -> { b: Double -> a * (1 + b / 100) } }
+
+            forAll<Int, Double> { a, b ->
+                a != 0 || partTwo(b, tax)(a) == tax(a)(b)
             }
         }
     }
--- src/test/kotlin/Funtions/Curring.kt
+++ src/test/kotlin/Funtions/Curring.kt
@@ -13,3 +13,5 @@ fun <A, B, C, D> FunctionBasedSpec.fourArgumentsCurring() = { a: A ->
 }
 
 fun <A, B, C> FunctionBasedSpec.partOne(a: A, f: (A) -> (B) -> C): (B) -> C = f(a)
+
+fun <A, B, C> FunctionBasedSpec.partTwo(b: B, f: (A) -> (B) -> C): (A) -> C = { a: A -> f(a)(b) }
