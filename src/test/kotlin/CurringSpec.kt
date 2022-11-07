da4e0a1a6354037b10cf3d344219f833c5f15032
Nok Jeong <35394575+nok3zy@users.noreply.github.com>
2022-11-07 14:27:04 +0900

    feat : add curring example (#1)
    * part one
    
    * add curring example

--- src/test/kotlin/CurringSpec.kt
+++ src/test/kotlin/CurringSpec.kt
@@ -1,7 +1,10 @@
 import Funtions.FunctionBasedSpec
 import Funtions.fourArgumentsCurring
+import Funtions.partOne
+import Funtions.partTwo
 import io.kotest.matchers.shouldBe
 import io.kotest.property.checkAll
+import io.kotest.property.forAll
 
 class CurringSpec : FunctionBasedSpec() {
     init {
@@ -10,6 +13,22 @@ class CurringSpec : FunctionBasedSpec() {
                 val result = fourArgumentsCurring<String, String, String, String>()(a)(b)(c)(d)()
                 val expected = "$a $b $c $d"
                 result shouldBe expected
+            }
+        }
+
+        "Fixed money then input rate"{
+            val tax = { a: Int -> { b: Double -> a * (1 + b / 100) } }
+
+            forAll<Int, Double> { a, b ->
+                a != 0 || partOne(a, tax)(b) == tax(a)(b)
+            }
+        }
+
+        "Fixed rate then input money"{
+            val tax = { a: Int -> { b: Double -> a * (1 + b / 100) } }
+
+            forAll<Int, Double> { a, b ->
+                a != 0 || partTwo(b, tax)(a) == tax(a)(b)
             }
         }
     }
